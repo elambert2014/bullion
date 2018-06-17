81e92ab63a64e85887540ff609d140203ed98b9d
malafaya <malafaya@clix.pt>
2018-06-17 22:22:06 +0100

    Version 3.0.1 release files

--- src/build.h
+++ src/build.h
@@ -1,3 +1,3 @@
-#define BUILD_SUFFIX ddd8800
-#define BUILD_DATE "2018-06-09 15:30:00 +0100"
+#define BUILD_SUFFIX 3018c01
+#define BUILD_DATE "2018-06-18 00:19:33 +0100"
 // should be auto generated but used to test compilation
